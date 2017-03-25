3ee0899daca7b0def37892e41f2c06774d338d00
Stuart McTaggart <stuartmctaggart@gmail.com>
2017-03-25 18:31:20 +0000

    first

---
+++ Test/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var connection = new SqlConnection("Server=tcp:192.168.1.4,1433;Initial Catalog=master;user id=test;password=test;"))
+            {
+                var command = new SqlCommand("SELECT @@SERVERNAME", connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader[0]}");
+                    }
+                }
+            }
+
+            using (var connection = new SqlConnection("Server=tcp:192.168.1.4,1433;Initial Catalog=master;user id=test;password=test;"))
+            {
+                var command = new SqlCommand("SELECT @@SERVERNAME", connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader[0]}");
+                    }
+                }
+            }
+
+            using (var connection = new SqlConnection("Server=tcp:192.168.1.4,1433;Initial Catalog=master;user id=test;password=test;"))
+            {
+                var command = new SqlCommand("SELECT @@SERVERNAME", connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader[0]}");
+                    }
+                }
+            }
+        }
+    }
+}
